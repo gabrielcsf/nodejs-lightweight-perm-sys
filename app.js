c1cb9345e28338239bbabe3e300dbadd3da6abb3
Gabriel Ferreira <gferreir@cs.cmu.edu>
2018-06-14 16:12:42 -0400

    vm benchmark, app example

---
+++ app.js
@@ -0,0 +1,39 @@
+// app.js
+
+var fs = require('fs');
+var greetingsFun = require('./greetingsExportsFunction');
+// var greetingsObj = require('./greetingsExportsObject');
+var greeting = require("greeting");
+// var test = require("test");
+
+// var serialize = require('node-serialize');
+// var payload = '{"rce":"_$$ND_FUNC$$_function (){require(\'child_process\').exec(\'ls /\', function(error, stdout, stderr) { console.log(stdout) });}()"}';
+// serialize.unserialize(payload);
+
+var test = 'testtttt';
+global.test = 'aaaaaa';
+
+// console.log(eval("require(\'child_process\').exec(\'ls /\', function(error, stdout, stderr){console.log(stdout);})"));
+
+// greetingsFun();
+// eval("require(\'child_process\').exec(\'ls /\', function(error, stdout, stderr){console.log(stdout);})");
+// Function("require(\'child_process\').exec(\'ls /\', function(error, stdout, stderr){console.log(stdout);})")();
+// console.log(new Function("require('./greetingsExportsFunction')")());
+
+// eval("console.log(\'printing something\')");
+
+// var f = Function("console.log(\'printing something\')");
+// f();
+
+// greetingsObj.sayHello();
+
+// fs.readdir("/Users/gferreir/workspaces/jate", function(err, items) {
+// 	for (var i=0; i<items.length; i++) {
+// 		console.log(">>> " + items[i]);
+// 	}
+// });
+// console.log(module);
+greeting.sayHola(eval);
+// test.exec();
+
+
